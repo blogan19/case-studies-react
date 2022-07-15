c36ddf39b13fd6ce3e47a0c5f314b627e4a1274f
blogan19 <you@example.com>
2022-07-15 07:10:05 +0100

    3rd push

--- src/components/Patient_details.jsx
+++ src/components/Patient_details.jsx
@@ -6,31 +6,30 @@ import Container from 'react-bootstrap/Container';
 const PatientDetails = (props) => {
     return(
         <Container>
-            <p>{props.demographics}</p>
-            <Table bordered hover>
+            <Table bordered>
                 <tbody>
                 <tr>
                     <td>
                         <i className="text-muted">Name  </i> 
-                        {props.firstname} {props.lastname}
+                        {props.patient.firstname} {props.patient.lastname}
                     </td>
                     <td>
                         <i className="text-muted">Hospital No  </i>
-                        {props.hospitalNo}
+                        {props.patient.hospitalNo}
                     </td>
                     <td>
                         <i className="text-muted">DoB  </i> 
-                        {props.dob}
+                        {props.patient.dob}
                     </td>
                 </tr>      
                 <tr>
                     <td>
                         <i className="text-muted">Weight  </i> 
-                        {props.weight}
+                        {props.patient.weight}
                     </td>
                     <td>
                         <i className="text-muted">Height  </i> 
-                        {props.height}
+                        {props.patient.height}
                     </td>
                     <td>
                         <i className="text-muted">Allergies  </i> 
@@ -40,7 +39,7 @@ const PatientDetails = (props) => {
                 <tr>
                     <td colSpan={3}>
                         <i className="text-muted">Address </i> 
-                        {props.address}
+                        {props.patient.address}
                     </td>
                 </tr>  
                 </tbody>
