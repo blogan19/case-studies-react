a2575ba17cd1fb990524e46f94070d5840106417
blogan19 <you@example.com>
2022-11-23 20:29:47 +0000

    route removed v2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
